d4e6ac81592e0bbf50e2d8d5cd7911b554b03c74
RenataSz4 <renatasan0411@gmail.com>
2026-04-22 19:27:18 -0700

    add initial backend

---
+++ backend/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, WebSocket
+from fastapi.websockets import WebSocketDisconnect
+
+from preprocess import preprocess_image
+from inference import predict
+from alerts import update_categories, should_send_alert
+
+app = FastAPI()
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+
+    try:
+        while True:
+            data = await websocket.receive_json()
+            
+            if "image" in data:
+                image_data = data["image"]
+                preprocessed_image = preprocess_image(image_data)
+                prediction = predict(preprocessed_image)
+                alert = should_send_alert(prediction)
+                
+                
+                await websocket.send_json({
+                    "detection": prediction,
+                    "alert": alert
+                })
+            
+            elif "categories" in data:
+                categories = data["categories"]
+                update_categories(categories)
+    except WebSocketDisconnect:
+        print("WebSocket disconnected")
